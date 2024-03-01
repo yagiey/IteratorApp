59763c103e5ba3cf10ec3fcc509714db473bec54
yagiey <yagiey@gmail.com>
2024-03-01 19:36:38 +0900

    change project settings

--- IteratorApp/Program.cs
+++ IteratorApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace IteratorApp
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
